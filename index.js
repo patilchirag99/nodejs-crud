1d052b4fcab267778b2f3e76cf1f6b0aff8f128f
CHIRAG <patil.chirag99@gmail.com>
2024-03-06 21:45:10 +0000

    Crud operations added

--- index.js
+++ index.js
@@ -1,47 +1,68 @@
-const fs = require('fs');
-const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-const products = data.products;
+const fs = require("fs");
+const express = require("express");
+const morgan = require("morgan");
 
-const express = require('express');
-const morgan = require('morgan');
+//Read data from file
+const index = fs.readFileSync("index.html", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+let products = data.products;
+
 const server = express();
 
 //bodyParser
 server.use(express.json());
 //for logging
-server.use(morgan('default'))
-server.use(express.static('public'));
-
+server.use(morgan("default"));
+//create static resource folder that can be accessed directly from browser
+server.use(express.static("public"));
 
 // API - Endpoint - Route
 
 //Products
+//API ROOT , base url
 
-server.get('/products', (req, res) => {
-  
+//Get all products
+server.get("/products", (req, res) => {
   res.json(products);
 });
-server.post('/', (req, res) => {
-  res.json({ type: 'POST' });
-});
-server.put('/', (req, res) => {
-  res.json({ type: 'PUT' });
-});
-server.delete('/', (req, res) => {
-  res.json({ type: 'DELETE' });
-});
-server.patch('/', (req, res) => {
-  res.json({ type: 'PATCH' });
+
+//Get product by id
+server.get("/products/:id", (req, res) => {
+  const id = +req.params.id; //Adding + to convert string id to number id
+  const productById = products.find((p) => p.id == id);
+  res.json(productById);
 });
 
-server.get('/demo', (req, res) => {
-  // res.sendStatus(404);
-  // res.json(products)
-  // res.status(201).send('<h1>hello</h1>')
-  // res.sendFile('/Users/abhishekrathore/Desktop/node-app/index.html')
+//Create products
+server.post("/products", (req, res) => {
+  products.push(req.body);
+  res.status(201).json(req.body);
 });
+
+//Delete  product by id
+server.delete("/products/:id", (req, res) => {
+  let id = +req.params.id;
+  products = products.filter((p) => p.id !== id);
+  res.json(req.body);
+});
+
+//Update product by id - overrides all the properties
+server.put("/products/:id", (req, res) => {
+  let id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id == id);
+  products.splice(productIndex,1,{...req.body, id:id})
+  res.json({product:"Updated"});
+});
+
+//Patch product by id - Just updates the values that are sent in request
+server.patch("/products/:id", (req, res) => {
+    let id = +req.params.id;
+    const productIndex = products.findIndex((p) => p.id == id);
+    const productOld = products[productIndex]
+    products.splice(productIndex,1,{...productOld , ...req.body,})
+    res.json({product:"Updated"});
+  });
 
 server.listen(8086, () => {
-  console.log('server started');
+  console.log("server started");
 });
