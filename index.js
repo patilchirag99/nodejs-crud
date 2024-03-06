55d9983a8afdea40cba7b09591c29f54504b5eee
CHIRAG <patil.chirag99@gmail.com>
2024-03-06 21:07:59 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const index = fs.readFileSync('index.html', 'utf-8');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const products = data.products;
+
+const express = require('express');
+const morgan = require('morgan');
+const server = express();
+
+//bodyParser
+server.use(express.json());
+//for logging
+server.use(morgan('default'))
+server.use(express.static('public'));
+
+
+// API - Endpoint - Route
+
+//Products
+
+server.get('/products', (req, res) => {
+  
+  res.json(products);
+});
+server.post('/', (req, res) => {
+  res.json({ type: 'POST' });
+});
+server.put('/', (req, res) => {
+  res.json({ type: 'PUT' });
+});
+server.delete('/', (req, res) => {
+  res.json({ type: 'DELETE' });
+});
+server.patch('/', (req, res) => {
+  res.json({ type: 'PATCH' });
+});
+
+server.get('/demo', (req, res) => {
+  // res.sendStatus(404);
+  // res.json(products)
+  // res.status(201).send('<h1>hello</h1>')
+  // res.sendFile('/Users/abhishekrathore/Desktop/node-app/index.html')
+});
+
+server.listen(8086, () => {
+  console.log('server started');
+});
